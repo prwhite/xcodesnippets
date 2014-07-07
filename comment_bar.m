670212d2ff575da4734681d108ca0691492dab3f
Payton White <payton@prehiti.net>
2014-07-07 11:15:47 -0700

    Imported user code snippets

---
+++ comment_bar.m
@@ -0,0 +1,10 @@
+// Comment Bar
+// 
+//
+// IDECodeSnippetCompletionPrefix: cbar
+// IDECodeSnippetCompletionScopes: [All]
+// IDECodeSnippetIdentifier: F6E615EF-183B-4EFF-81FB-1DC4822D2C11
+// IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C-Plus-Plus
+// IDECodeSnippetUserSnippet: 1
+// IDECodeSnippetVersion: 2
+// ///////////////////////////////////////////////////////////////////
---
+++ headerdoc_comment_block.m
@@ -0,0 +1,12 @@
+// HeaderDoc Comment Block
+// 
+//
+// IDECodeSnippetCompletionPrefix: cblock
+// IDECodeSnippetCompletionScopes: [All]
+// IDECodeSnippetIdentifier: 934C9B3F-C460-43A0-B500-836BDD7E7E8D
+// IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C-Plus-Plus
+// IDECodeSnippetUserSnippet: 1
+// IDECodeSnippetVersion: 2
+/**
+  <#comment#>
+*/
